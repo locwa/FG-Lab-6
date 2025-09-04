3aad1a75b06c3463846d3226429432833204a2fc
locwa <94048557+locwa@users.noreply.github.com.>
2025-09-04 19:00:19 +0800

    added product list as json and implemented it to product-list component

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router'
 import { CommonModule } from '@angular/common'
+import '../../../products.json'
+
+import productsData from '../../../products.json';
 
 
 @Component({
@@ -10,42 +13,5 @@ import { CommonModule } from '@angular/common'
   styleUrl: './product-list.component.css'
 })
 export class ProductListComponent {
-  products = [
-    {
-      id: 1,
-      name: "Iphone 6s",
-      price: 4000,
-      location: "SM Baguio",
-      condition: "Good"
-    },
-    {
-      id: 2,
-      name: "Iphone X",
-      price: 6000,
-      location: "SM San Fernando",
-      condition: "Good"
-    },
-    {
-      id: 3,
-      name: "Iphone Xs",
-      price: 6500,
-      location: "SM Baguio",
-      condition: "Good"
-    },
-    {
-      id: 4,
-      name: "Iphone XR",
-      price: 6500,
-      location: "SM Tarlac",
-      condition: "Good"
-    },
-    {
-      id: 5,
-      name: "Iphone 12 Pro Max",
-      price: 12000,
-      location: "SM Rosales",
-      condition: "Good"
-    },
-
-  ]
+  products = productsData;
 }
