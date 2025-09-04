f3316419079565b089442eefee8240524970dbf1
locwa <94048557+locwa@users.noreply.github.com.>
2025-09-04 20:47:19 +0800

    refined product-details component

--- src/app/product-details/product-details.component.ts
+++ src/app/product-details/product-details.component.ts
@@ -3,6 +3,8 @@ import { ActivatedRoute } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
+import productsData from '../../../products.json';
+
 @Component({
   selector: 'app-product-details',
   standalone: true,
@@ -13,43 +15,7 @@ import { RouterModule } from '@angular/router';
 export class ProductDetailsComponent {
   productId: number | null = null;
   product: any = null;
-  productList = [
-    {
-      id: 1,
-      name: "Iphone 6s",
-      price: 4000,
-      location: "SM Baguio",
-      condition: "Good"
-    },
-    {
-      id: 2,
-      name: "Iphone X",
-      price: 6000,
-      location: "SM San Fernando",
-      condition: "Good"
-    },
-    {
-      id: 3,
-      name: "Iphone Xs",
-      price: 6500,
-      location: "SM Baguio",
-      condition: "Good"
-    },
-    {
-      id: 4,
-      name: "Iphone XR",
-      price: 6500,
-      location: "SM Tarlac",
-      condition: "Good"
-    },
-    {
-      id: 5,
-      name: "Iphone 12 Pro Max",
-      price: 12000,
-      location: "SM Rosales",
-      condition: "Good"
-    },
-  ]
+  productList = productsData;
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
